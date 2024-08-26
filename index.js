7c45a1400ec8f9aca6c55781954dbbcbddb3d071
kunj-31 <kunjdesai487@gmail.com>
2024-08-26 18:20:55 +0530

    thired commit

--- index.js
+++ index.js
@@ -1,20 +1,31 @@
+require("dotenv").config();
 const express = require("express");
 
 const server = express();
 const morgon = require("morgan");
+const mongoose = require("mongoose");
+
 server.use(express.json());
 const productRouter = require("./routes/product");
 const userRouter = require("./routes/user");
 
+console.log("env", process.env.DB_PASSWORD);
+
+//db connection
+main().catch(() => console.log());
+
+async function main() {
+  await mongoose.connect("mongodb://localhost:27017/ecommerce");
+
+  console.log("db connected");
+}
 
 //body parser
 server.use(morgon("default"));
 server.use(express.static("public"));
 server.use("/products", productRouter.router);
-server.use("/users",userRouter.router);
+server.use("/users", userRouter.router);
 
-//MVC model-view-controller
-
-server.listen(8080, () => {
-  console.log("server is running on port 8080");
+server.listen(process.env.PORT, () => {
+  console.log('server started');
 });
